81b03cc6eacd32b1451c9a7a28bde4fc6e705e02
Mac doussias <macdd@me.com>
2017-12-02 23:02:46 -0800

    Initial commit

---
+++ CST205_FINAL_MAKESMART.py
@@ -0,0 +1,49 @@
+# CST205 FINAL ASSIGNMENT
+# Completed by: Team MakeSmart
+# Pavlos Papadonikolakis, Maco Doussias, Jake McGhee
+
+# TITLE: RPG Character Generator
+# This program serves as the RPG character generator for a video game
+# Gets user to choose a character type (Wizard, Barbarian, Archer)
+# Gets user to choose a voice for the character (six different voices)
+# Gets user to choose a color for their character (Black, Red, Blue)
+# On completion of choosing character, plays characters voice and displays image
+
+#NOTE:  characterGenerator() function will run the program
+
+
+#TODO this is just preliminary code.... Make major changes to sructure or content as needed
+
+def chooseCharacterType(): # Wizard, Barbarian, Archer
+    #TODO make this function work
+    """ Prompts user to choose a character type and returns that type """
+
+def chooseCharacterColor(): # black, blue, blonde
+    #TODO make this function work
+    """ Shows user a slection of character color choices """
+    """ Prompts user to pick a color for their character """
+    """ Returns the character color """
+
+def chooseCharacterVoice(voice):
+    #TODO make this function work
+    """ Prompts user to choose a character voice """
+    """ manipulates a sound file and returns it """
+    newVoice = voice
+    # alter newVoice here
+    return newVoice
+
+def welcomeMessage():
+    #TODO make this function work
+    """ Displays a textbox welcome message to the user """
+    
+def characterGenerator():
+    #TODO make this function work
+    #TODO need to figure out how to have a characterImage and a characterType
+    """ Runs the RPG character generator"""
+    #
+    # characterVoice = makeSound(fileLocation)  
+    # welcomeMessage() #displays a welcome message to user 
+    # characterImage = chooseCharacterType()
+    # characterVoice = chooseCharacterVoice(characterVoice)
+    # characterColor = chooseCharacterColor()
+    # 

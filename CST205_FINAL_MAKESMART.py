d005f51da930ca7d825efa1add183de810095635
Mcghee-Jake <jmcghee@csumb.edu>
2017-12-08 12:00:33 -0800

    added in functionality to change the character color

--- CST205_FINAL_MAKESMART.py
+++ CST205_FINAL_MAKESMART.py
@@ -20,7 +20,7 @@ import os
 
 def getPicture(fileName):
     """ Returns a picture from the folder same folder that that the program is being run in """
-    """ If the picture is not found, it prompts the user to select a picture ""
+    """ If the picture is not found, it prompts the user to select a picture """
 
     # Get the programs working directory
     directory = os.path.dirname(__file__) 
@@ -35,6 +35,44 @@ def getPicture(fileName):
     else:
         showInformation("File not found\nPlease select " + fileName)
         return makePicture(pickAFile())
+        
+        
+def colorize(picOriginal, picColoredArea, targetColor, threshold, tintColor, multiplier):
+    """ Takes a picture and a reference picture and tints the area in the original picture based on the colored area in the reference picture """
+    """ 
+        Args:
+            picOriginal (pic): The picture that will be altered
+            picColoredArea (pic): The picture containing the colored area that will be referenced and tinted in picOriginal
+                                  Must be same width and height as picOriginal
+            targetColor (color): The color of the reference area in picColoredArea
+            threshold (int): The amount of leeway the function will give between the difference of the targetColor and the actual color in picColoredArea
+            tintColor (string): The color to tint to. Should be the result of chooseCharacterColor()
+            multiplier (float): The amount by which targeted pixels will be tinted. Should be a smallish number (probably somewhere between 1.5 and 3)
+    """
+            
+    # Scan picColoredArea for targetColor
+    for x in range(0, getWidth(picColoredArea)):
+        for y in range(0, getHeight(picColoredArea)):    
+            p = getPixel(picColoredArea, x, y)
+            color = getColor(p)
+            # If pixel is within the threshold of the targetColor
+            if distance(color, targetColor) <= threshold:
+                p = getPixel(picOriginal, x, y)
+                # tint that pixel
+                if tintColor == 'RED':
+                    if getRed(p) < 50:
+                        setRed(p, 50) 
+                    setRed(p, getRed(p)*multiplier)
+                elif tintColor == 'GREEN':
+                    if getGreen(p) < 50:
+                        setGreen(p, 50)
+                    setGreen(p, getGreen(p)*multiplier)
+                elif tintColor == 'BLUE':
+                    if getBlue(p) < 50:
+                        setBlue(p, 50)
+                    setBlue(p, getBlue(p)*multiplier)          
+    show(picOriginal)                                
+    return picOriginal   
 
 def getSelection(msg, list):
     """ Presents the user with a given list of options """
@@ -68,12 +106,12 @@ def chooseClass(): # Wizard, Barbarian, Archer
     return getSelection(msg, classes)
     
       
-def chooseCharacterColor(): # black, blue, blonde
+def chooseCharacterColor():
     """ Shows user a slection of character color choices """
     """ Prompts user to pick a color for their character """
     """ Returns the character color """
     
-    colors = ['BLACK', 'BLUE', 'BLONDE']
+    colors = ['RED', 'GREEN', 'BLUE']
     msg = "Please select a character color"
     return getSelection(msg, colors)
 
@@ -100,3 +138,8 @@ def characterGenerator():
     # characterVoice = chooseCharacterVoice(characterVoice)
     # characterColor = chooseCharacterColor()
     # 
+    
+    # Just testing the colorize function, this sohuld be edited later
+    # I made the wizard_example_colored in MS paint so the threshold will be way lower when I make the real photos in photoshop
+    tintColor = chooseCharacterColor()
+    colorize(getPicture("wizard_example.jpg"), getPicture("wizard_example_colored.jpg"), green, 200, tintColor, 2.0)
